93b2579f71fffc0c7e2728b08f334429f8f1aa18
James Hovell <Jhovell@tcadmins-MacBook-Air.local>
2020-03-03 11:10:09 +0000

    added different types of variables

---
+++ FirstWeekLearning.py
@@ -0,0 +1,19 @@
+# Variables
+# It is like a box. You give it a name, put stuff inside
+# defining variable book
+book = 'Rich dad poor dad'
+
+# Print function
+# Outputs content to the terminal (makes it visible)
+book
+print(book)
+
+# Type function
+# Allows us to check data types
+datatype_of_book = type(book)
+print(datatype_of_book)
+
+## Conventions
+# Lower case for variable naming with underscore
+
+
