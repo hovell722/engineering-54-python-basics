fec1d406aad6fcd04160ab52c133a2bad0717255
James Hovell <Jhovell@tcadmins-MacBook-Air.local>
2020-03-04 09:15:19 +0000

    exercise 106

--- exercise_106.py
+++ exercise_106.py
@@ -2,17 +2,24 @@
 # I want you to use operators
 # equate something
 
+sum_a = 5
+print(sum_a == 5)
+
 # As a user, I want to be able to guess a number and know if i got it correct or not, so that I can know if I won or not.
 
 
 # We should define/assign number to a variable called magic_number
 # magic_number =
 
+magic_number = 7
+
 # I need to ask user for input
 
+user_number = int(input("Guess a number. "))
 
 # I need to check if this input matches a magic_number
 
+print("That answer was " + str(user_number == magic_number) + "!")
 
 # I need to let the user know if the response was write or not
 #self five
